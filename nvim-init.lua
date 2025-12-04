f9f582328ee27ba6a06099311efaf80b081888e4
Johannes Thumshirn <johannes.thumshirn@wdc.com>
2025-12-04 11:34:38 +0100

    add nvim-init.lua

---
+++ nvim-init.lua
@@ -0,0 +1,39 @@
+--      .o oOOOOOOOo                                            OOOo
+--      Ob.OOOOOOOo  OOOo.      oOOo.                      .adOOOOOOO
+--      OboO"""""""""""".OOo. .oOOOOOo.    OOOo.oOOOOOo.."""""""""'OO
+--      OOP.oOOOOOOOOOOO "POOOOOOOOOOOo.   `"OOOOOOOOOP,OOOOOOOOOOOB'
+--      `O'OOOO'     `OOOOo"OOOOOOOOOOO` .adOOOOOOOOO"oOOO'    `OOOOo
+--      .OOOO'            `OOOOOOOOOOOOOOOOOOOOOOOOOO'            `OO
+--      OOOOO                 '"OOOOOOOOOOOOOOOO"`                oOO
+--     oOOOOOba.                .adOOOOOOOOOOba               .adOOOOo.
+--    oOOOOOOOOOOOOOba.    .adOOOOOOOOOO@^OOOOOOOba.     .adOOOOOOOOOOOO
+--   OOOOOOOOOOOOOOOOO.OOOOOOOOOOOOOO"`  '"OOOOOOOOOOOOO.OOOOOOOOOOOOOO
+--   "OOOO"       "YOoOOOOMOIONODOO"`  .   '"OOROAOPOEOOOoOY"     "OOO"
+--      Y           'OOOOOOOOOOOOOO: .oOOo. :OOOOOOOOOOO?'         :`
+--      :            .oO%OOOOOOOOOOo.OOOOOO.oOOOOOOOOOOOO?         .
+--      .            oOOP"%OOOOOOOOoOOOOOOO?oOOOOO?OOOO"OOo
+--                   '%o  OOOO"%OOOO%"%OOOOO"OOOOOO"OOO':
+--                        `$"  `OOOO' `O"Y ' `OOOO'  o             .
+--      .                  .     OP"          : o     .
+--                                :
+--                                .
+
+-- Optics
+vim.opt.background = 'dark'
+vim.cmd.colorscheme("habamax")
+vim.opt.termguicolors = true
+
+-- Coding
+vim.opt.tabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.softtabstop = 8
+vim.opt.smartindent = true
+
+-- Behaviour
+vim.opt.backup = false
+vim.opt.hlsearch = true
+vim.opt.mouse = "a"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.spell = true
+vim.opt.wrap = false
