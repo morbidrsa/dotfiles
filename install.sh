dcc0051c53e979368f34cdc06fd837709f4f5aa0
Johannes Thumshirn <jth@kernel.org>
2025-12-26 11:38:12 +0100

    add messages to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+echo "Installing tmux.conf to $HOME/.tmux.conf"
 cp $PWD/tmux.conf $HOME/.tmux.conf
+
+echo "Installing nvim-init.lua to $HOME/.config/nvim/init.lua"
 cp $PWD/nvim-init.lua $HOME/.config/nvim/init.lua
